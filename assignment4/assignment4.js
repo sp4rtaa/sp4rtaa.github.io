3116b485a41371fcc09c0ea127c371bdc1d219d2
sp4rtaa <105113470+sp4rtaa@users.noreply.github.com>
2022-05-21 09:04:58 -0400

    Add files via upload

--- assignment4/assignment4.js
+++ assignment4/assignment4.js
@@ -6,16 +6,15 @@ const lucasWeight = (92)
 const lucasHeight = (1.95)
 var peterBmi = (peterWeight / (peterHeight * peterHeight));
 var lucasBmi = (lucasWeight / (lucasHeight * lucasHeight));
-console.log(peterBmi + ' Is Peters BMI');
-console.log(lucasBmi + ' is Lucases BMI');
-console.log(lucasBmi > peterBmi);
+var lucasCompared = (lucasBmi>peterBmi);
+console.log( 'Peters BMI is ' + peterBmi + ', Lucases BMI is ' + lucasBmi + ', Peters BMI is higher than Lucases ' + lucasCompared);
 
 // second //
 
-var temperatureC = '20C';
-temperatureC = '52F';
-var temperatureF = 'temperatureC'
-temperatureC = "20C"
+var temperatureC = (20 + 32) * 1/8;
+console.log ('20 °C is '+(temperatureC)+'°F');
+var temperatureF = (79 - 32) / 1.8;
+console.log ('79 °F is '+(temperatureF)+'°C');
 
 //third//
 if  (lucasBmi > peterBmi) {console.log('Lucases BMI is higher that Peters. Lucas is the man!');}
@@ -26,11 +25,14 @@ console.log(`Peters BMI  ${peterBmi} is higher than Lucases BMI ${lucasBmi}!`);
 //fourth//
 
 
-function celsiusToFahrenheit(val1, val2, val3) {
-    
-    let new =((val1+fahr) (val2+fahr) (val3+fahr));
-    const fahr = 32;
-    return `your temperature in Fahrenheit is` ${new};
- }
- 
- console.log(celsiusToFahrenheit (100, 0, 10));
+let tempConvertingC = (val1) =>  (val1 * 1.8) +32;
+
+console.log('100 °C is ' + tempConvertingC(100) + ' °F');
+console.log('0 °C is ' + tempConvertingC(0) + ' °F')
+console.log('10 °C is ' + tempConvertingC(10) + '°F')
+
+let tempConvertingF = (val2) => (val2 / 1.8) / 32;
+
+console.log('32 °F is ' + tempConvertingF(32) + ' °C')
+console.log('12 °F is ' + tempConvertingF(12) + ' °C')
+console.log('102 °F is ' + tempConvertingF(102) + ' °C')
